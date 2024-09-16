6c40457f18ea44d792fa9b1ded5084bab260fa9e
byeonjuhwan <hcg.byeonjuhwan@gmail.com>
2024-09-17 00:20:20 +0900

    apiGateWay -> 로드밸런싱

--- first-service/src/main/kotlin/dev/be/controller/FirstServiceController.kt
+++ first-service/src/main/kotlin/dev/be/controller/FirstServiceController.kt
@@ -1,5 +1,8 @@
 package dev.be.controller
 
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/first-service")
 class FirstServiceController {
+
+    private val log: Logger = LoggerFactory.getLogger(FirstServiceController::class.java)
 
     @GetMapping("/welcome")
     fun welcome(): String {
@@ -21,7 +26,8 @@ class FirstServiceController {
     }
 
     @GetMapping("/check")
-    fun check () : String {
-        return "Hi, there. this is a message from first service"
+    fun check(request: HttpServletRequest): String {
+        log.info("Server Port = {}", request.serverPort.toString())
+        return "Hi, there. this is a message from first service on Port ${request.serverPort}"
     }
 }
